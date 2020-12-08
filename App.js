ac02b4ef52ea2f7c95338c1c57168ec67ca8a56d
Tehman Sardar <tehmansardar@gmail.com>
2020-12-08 22:28:07 +0500

    Create Header Component

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import {View, Text, Image, StyleSheet} from 'react-native'
+import {View, StyleSheet} from 'react-native'
+import Header from './components/Header'
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={{color:'darkslateblue', fontSize:30}}>Hello world</Text>
-      <Image style={styles.img} source={{uri:'https://post.greatist.com/wp-content/uploads/sites/3/2020/02/325466_1100-1100x628.jpg'}} />
+      <Header title="Shopping List" />
     </View>
   )
 }
@@ -12,14 +12,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    justifyContent:'center',
-    alignItems:'center'
   },
-  img:{
-    width:100,
-    height:100,
-    borderRadius: 100/2
-  }
 
 })
 
